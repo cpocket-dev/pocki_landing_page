e3a90e493810876c331da8c789b65edc74685e1e
VelasquezDev <alejandro.velasquez@cpocket.global>
2023-12-14 15:24:35 -0500

    Tailwind Setup
    - Se configuraron los parametros estandar de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        primary: '#80bc00',
+        secondary: '#749414',
+        terciary: '#6a6161',
+      },
+      width: {
+        'swiperCont': '45rem',
+      }
+    },
   },
   plugins: [],
 }
