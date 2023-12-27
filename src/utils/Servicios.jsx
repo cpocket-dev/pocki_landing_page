d330c6a8b80333d5e56c87fc9e339fb6e4e5cc9d
VelasquezDev <150847054+VelasquezDev@users.noreply.github.com>
2023-12-27 17:33:57 -0500

    UI Update

--- src/utils/Servicios.jsx
+++ src/utils/Servicios.jsx
@@ -1,32 +1,53 @@
 import * as Images from "../assets/services/Images";
 import { GetCards } from "../data/dataCards";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Servicios = () => {
   const services = GetCards();
 
   return (
     <>
-      {services.map((s) => {
-        const ServiceImage = Images[s.serviceImage]; // Access the correct component from the imported Images
-        const ServiceBg = Images[s.serviceBg]; // Access the correct component from the imported bg Images
-        return (
-          <div
-            key={s.id}
-            className="w-full flex flex-col items-center text-center px-[1rem] h-full justify-center content-center gap-1"
-          >
-            <p className="overflow-hidden h-[3rem]">{s.title}</p>
-            <div className="service-images relative h-[45%] min-w-full w-[13rem] z-[8] overflow-hidden hue-rotate-[326deg]">
-              <div className="services-bg absolute w-full h-full z-[1] overflow-hidden">
-                {ServiceBg && <ServiceBg />}
+      <Swiper
+        slidesPerView={1}
+        cssMode={true}
+        direction="horizontal"
+        modules={[Autoplay]}
+        className="h-full overflow-hidden"
+        autoplay={{
+          delay: 800000,
+          disableOnInteraction: false,
+        }}
+        /* data-aos="flip-left"
+        data-aos-once="true"
+        data-aos-delay="500"
+        data-aos-duration="1000" */
+      >
+        {services.map((s, index) => {
+          const ServiceImage = Images[s.serviceImage]; // Access the correct component from the imported Images
+          const ServiceBg = Images[s.serviceBg]; // Access the correct component from the imported bg Images
+
+          return (
+            <SwiperSlide key={index}>
+              <p className="flex justify-center items-center text-lg font-bold z-[2] h-[10%] w-full text-center bg-black text-white">{s.title}</p>
+              <div className={`services-image-cont w-full h-[80%] translate-y-4 px-8 flex justify-center items-center relative text-center overflow-hidden -z-10 services${index}`}>
+                <div className="services-svg flex w-[80%] h-[90%] translate-y-4 scale-[0.9] items-center content-center overflow-visible justify-center rounded-lg">
+                  {ServiceImage && (
+                    <ServiceImage className="services-img absolute max-h-[130%] overflow-visible" />
+                  )}
+                  {ServiceBg && (
+                    <ServiceBg className="services-bg max-h-[130%] absolute overflow-visible" />
+                  )}
+                </div>
+                <p className={`services-text services-text${index} z-[1] h-max rounded-lg p-4 max-w-[30%] bg-black odd:translate-x-[-3rem] text-white right-0`}>{s.text}</p>
               </div>
-              <div className="services-image z-[4] overflow-hidden">
-                {ServiceImage && <ServiceImage />}
-              </div>
-            </div>
-            <p className="overflow-visible h-[33%]">{s.text}</p>
-          </div>
-        );
-      })}
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </>
   );
 };
